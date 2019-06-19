871f78f49a66a8c34b515ae2c82564ab9dfa8dcc
smhailey <smhailey@msn.com>
2019-06-19 13:57:33 -0600

    Files created.

---
+++ app/models/Car.js
@@ -0,0 +1,19 @@
+export default class Car {
+  constructor(data) {
+    this.make = data.make || ''
+    this.model = data.model || ''
+    this.year = data.year || ''
+    this.color = data.color || ''
+  }
+
+  get Template() {
+    return `
+    <div class="col-4">
+        <h4>Make: ${this.make}</h4>
+        <h4>Model: ${this.model}</h4>
+        <h4>Year: ${this.year}</h4>
+        <p>Color: ${this.color}</p>
+      </div>
+    `
+  }
+}
